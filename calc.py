8662b5c4256533f0127dee47f228221ad01ad9fa
hank <hank@jointsecurityarea.org>
2019-01-01 19:03:34 -0500

    Update calc.py
    Adding row delete

--- calc.py
+++ calc.py
@@ -52,9 +52,7 @@ wb = load_workbook(filename=args.spreadsheet,
     data_only=True)
 players_sheet = wb['Players']
 
-players_data = []
-for row in players_sheet.iter_rows():
-    players_data.append(row)
+players_data = players_sheet.rows
 
 player = None
 scores = []
@@ -63,8 +61,10 @@ header_row = False
 score_rows = False
 final_players = {}
 pp = pprint.PrettyPrinter(indent=4)
+row_replacements = []
+player_row_starts = []
 try:
-    for r in players_data:
+    for idx, r in enumerate(players_data):
         # print(f"Row: {[x.value for x in r]}")
         if header_row:
             # print("Header row")
@@ -83,13 +83,27 @@ try:
             print(f"Processing player: {player}")
             # print([x.value for x in r])
             data_row = True
-        elif score_rows and r[1] is not None and r[1].value is None:
+        elif score_rows and (r[0].value == 21 or (r[1] is not None and r[1].value is None)):
+            # If we have a final value, add it for consideration
+            if r[0].value == 21 and r[1].value is not None:
+                # print(f"Row: {[x.value for x in r]}")
+                # print(f"Adding last score")
+                row_idx = idx+1
+                scores.append((row_idx, [x.value for x in r]))
             # print(f"Finished scores for {player}")
             # First, if there are more than 20 scores, find the
             # 20 most recent
+            player_row_starts.append(scores[0][0])
             if len(scores) > 20:
-                sorted_by_date = sorted(scores, key=lambda x: x[1])
-                scores = sorted_by_date[0:20]
+                # print(len(scores))
+                sorted_by_date = sorted(scores, key=lambda x: x[1][1])
+                # pp.pprint([x[1][1] for x in sorted_by_date])
+                # print(f"Will replace row {sorted_by_date[0][0]} with {scores[20][0]}")
+                row_replacements.append((scores[20][0], sorted_by_date[0][0]))
+                scores = [x[1] for x in sorted_by_date][1:20]
+            else:
+                # Knock off indexes
+                scores = [x[1] for x in scores]
             # Calculate new values
             sorted_by_score = sorted(scores, key=lambda x: x[4])
             differentials = [x[8] for x in sorted_by_score]
@@ -110,8 +124,9 @@ try:
             scores = []
         elif score_rows:
             # Scores
-            # print(f"Scores: {[x.value for x in r]}")
-            scores.append([x.value for x in r])
+            row_idx = idx+1
+            # print(f"Scores {row_idx}: {[x.value for x in r]}")
+            scores.append((row_idx, [x.value for x in r]))
         else:
             pass
 except:
@@ -123,10 +138,32 @@ except:
 wb.close()
 wb = load_workbook(filename=args.spreadsheet)
 players_sheet = wb['Players']
+# Update player data
 for player, data in final_players.items():
     # print(player, data)
     for k, v in data.items():
         players_sheet[k] = v
+# Remove rows and renumber
+for rd in row_replacements:
+    print(f"Replacing row {rd[1]} with {rd[0]}")
+    for l in ("B", "C", "D", "E", "F"):
+        players_sheet[l + str(rd[1])] = players_sheet[l + str(rd[0])].value
+        players_sheet[l + str(rd[0])] = ""
+    # Shift all number rows
+    # nprs = []
+    # for f in player_row_starts:
+    #     if f > rd:
+    #         nprs.append(f-1)
+    #     else:
+    #         nprs.append(f)
+    # player_row_starts = nprs
+
+# Update player row numbering
+for f in player_row_starts:
+    for i in range(f, f+21):
+        # print(f"Updating A{i} to {i-f+1}")
+        players_sheet[f"A{i}"] = i-f+1
+
 # Save out spreadsheet
 if args.o is None:
     # Overwrite
